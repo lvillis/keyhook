22ae3431b91e7ddc4d95f46f74acc9c5936516ba
lvillis <56720445+lvillis@users.noreply.github.com>
2025-06-08 17:02:17 +0800

    chore: apply rustfmt

--- crates/keyhook-ui/src/app.rs
+++ crates/keyhook-ui/src/app.rs
@@ -57,7 +57,7 @@ fn push_toast(msg: &str) {
     let cb = Closure::<dyn FnOnce()>::once({
         let div = div.clone();
         move || {
-           div.remove();
+            div.remove();
         }
     });
     win.set_timeout_with_callback_and_timeout_and_arguments_0(cb.as_ref().unchecked_ref(), 3000)
